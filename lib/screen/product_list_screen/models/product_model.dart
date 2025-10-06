785236309f137632a6a3e60e8a554af589d09ac6
Rahat <nahidrahat007@gmail.com>
2025-10-06 11:03:28 +0600

    .

---
+++ lib/screen/product_list_screen/models/product_model.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+part 'product_model.g.dart';
+
+@HiveType(typeId: 0)
+class Product extends HiveObject {
+  @HiveField(0)
+  int id;
+
+  @HiveField(1)
+  String title;
+
+  @HiveField(2)
+  String image;
+
+  Product({required this.id, required this.title, required this.image});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(id: json['id'], title: json['title'], image: json['image']);
+  }
+}
